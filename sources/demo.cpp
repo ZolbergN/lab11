e84046f8a608d657dc5a5051156df8016614b650
Roman <exculibur@rambler.ru>
2017-12-06 19:35:00 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,35 +1,80 @@
+#include <print.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
-#include <iostream>
 
+using namespace boost::filesystem;
 using namespace boost::program_options;
 
-void on_age(int age)
+int main(int argc, char** argv)
 {
-  std::cout << "On age: " << age << '\n';
-}
+try
+{
+	std::string text;
 
-int main(int argc, const char *argv[])
-{
-  try
-  {
-    options_description desc{"Options"};
+    options_description desc{"General options"};
     desc.add_options()
       ("help,h", "Help screen")
-      ("pi", value<float>()->default_value(3.14f), "Pi")
-      ("age", value<int>()->notifier(on_age), "Age");
+      ("output,o", value<std::string>(), "Output");
 
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
+
+    std::string pAddress = getenv("HOME");
+    pAddress += "/.config/demo.cfg";
+    const char* _pAddress = pAddress.c_str();
+
+
+    if(exists(pAddress))
+    {
+    	store(parse_config_file<char>( _pAddress, desc ), vm);
+    }
     notify(vm);
 
     if (vm.count("help"))
+    {
       std::cout << desc << '\n';
-    else if (vm.count("age"))
-      std::cout << "Age: " << vm["age"].as<int>() << '\n';
-    else if (vm.count("pi"))
-      std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
-  }
-  catch (const error &ex)
+    }
+
+    else if (getenv("DEMO_OUTPUT") != nullptr)
+    {
+
+
+        std::string _trAddress = getenv("DEMO_OUTPUT");
+        std::ofstream out(_trAddress, std::ios_base::app);
+        while(std::cin >> text)
+         {
+			print(text, out);
+			out << std::endl;
+		}
+        out.close();
+  	}
+
+  	else if (vm.count("output"))
+    {
+
+  		std::ofstream out(vm["output"].as<std::string>(), std::ios_base::app);
+		while(std::cin >> text)
+    {
+			print(text, out);
+			out << std::endl;
+		}
+		out.close();
+  	}
+
+  	else
+    {
+  		std::cout << "DEFAULT" << std::endl;
+
+		std::ofstream out("default.log", std::ios_base::app);
+		while(std::cin >> text)
+     {
+			print(text, out);
+			out << std::endl;
+		}
+		out.close();
+  	}
+
+  } catch (const error &ex)
   {
     std::cerr << ex.what() << '\n';
   }
